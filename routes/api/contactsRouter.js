13225838d4f39b1acea0fc6d7471f88a88793a49
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-03-30 15:33:02 +0100

    Update contactsRouter.js

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,23 +1,51 @@
-import express from "express";
-import {
-  listContact,
-  getContactById,
-  deleteContactById,
-  createContact,
-  updateContactById,
-} from "../../controllers/contactsControllers.js";
-import { getContactById } from "../../services/contactsServices.js";
+// import express from "express";
+// import {
+//   listContact,
+//   getContactById,
+//   deleteContactById,
+//   createContact,
+//   updateContactById,
+// } from "../../controllers/contactsControllers.js";
+// import { getContactById } from "../../services/contactsServices.js";
 
-const contactsRouter = express.Router();
+// const contactsRouter = express.Router();
 
-contactsRouter.get("/", listContact);
+// contactsRouter.get("/", listContact);
 
-contactsRouter.get("/:id", getContactById);
+// contactsRouter.get("/:id", getContactById);
 
-contactsRouter.delete("/:id", deleteContactById);
+// contactsRouter.delete("/:id", deleteContactById);
 
-contactsRouter.post("/", createContact);
+// contactsRouter.post("/", createContact);
 
-contactsRouter.put("/:id", updateContactById);
+// contactsRouter.put("/:id", updateContactById);
 
-export default contactsRouter;
+// export default contactsRouter;
+const express = require('express')
+
+const contacts = require("../../models/contacts")
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  const result = await contacts.listContacts();
+  res.json(result)
+})
+
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+module.exports = router
